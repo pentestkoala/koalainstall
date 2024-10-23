bd26356aec7243fb50204a781d55bbf1733cc18e
Pentestkoala <66334912+pentestkoala@users.noreply.github.com>
2024-10-23 11:38:14 +0200

    Update koalainstall.py

--- koalainstall.py
+++ koalainstall.py
@@ -1,42 +1,55 @@
 import os
-
-def install():
-	print("[+] Install Seclists..")
-	os.system("git clone https://github.com/danielmiessler/SecLists /usr/share/wordlists/SecLists")
-
-	path = "/opt"
-	print("[+] Create all the directories")
-	os.system('mkdir ' + str(path) + 'privesc')
-	os.system('mkdir ' + str(path) + 'shells')
-	os.system('mkdir ' + str(path) + 'privesc/linux')
-	os.system('mkdir ' + str(path) + 'privesc/windows')
-
-	print("[+] Install static binaries..")
-	os.system("git clone https://github.com/andrew-d/static-binaries " + str(path) + "/static-binaries")
-
-	print("[+] Install evilwinrm..")
-	os.system("git clone https://github.com/Hackplayers/evil-winrm " + str(path) + "/shells/evilwin-rm")
-
-	print("[+] Install nishang..")
-	os.system("git clone https://github.com/samratashok/nishang " + str(path) + "/shells/nishang")
-
-	print("[+] Install JAWS..")
-	os.system("git clone https://github.com/411Hall/JAWS " + str(path) + "/privesc/windows/JAWS")
-
-	print("[+] Install PowerTools..")
-	os.system("git clone https://github.com/PowerShellMafia/PowerSploit/ " + str(path) + "/privesc/windows/PowerTools")
-
-	print("[+] Windows-Exploit-Suggester..")
-	os.system("git clone https://github.com/AonCyberLabs/Windows-Exploit-Suggester " + str(path) + "/privesc/windows/windows-exploit-suggester")
-
-	print("[+] LinEnum..")
-	os.system("git clone https://github.com/rebootuser/LinEnum " + str(path) + "/privesc/linux/LinEnum")
-
-	print("[+] Suid3num..")
-	os.system("git clone https://github.com/Anon-Exploiter/SUID3NUM " + str(path) + "/privesc/linux/Suid3num")
-
-	print("[+] Privilege-escalation-awesome-scripts-suite..")
-	os.system("git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite " + str(path) + "/privesc")
-
-if __name__=="__main__":
-	install()
+import subprocess
+ 
+# Function to run a shell command
+def run_command(command, cwd=None):
+    try:
+        subprocess.run(command, shell=True, check=True, cwd=cwd)
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed: {command}\nError: {e}")
+ 
+# Update the system's package list
+print("Updating package list...")
+run_command("sudo apt update -y")
+ 
+# List of GitHub repositories to clone
+repositories = [
+    "https://github.com/danielmiessler/SecLists",
+    "https://github.com/peass-ng/PEASS-ng/tree/master/winPEAS",
+    "https://github.com/peass-ng/PEASS-ng/tree/master/linPEAS",
+    "https://github.com/itm4n/PrivescCheck",
+    "https://github.com/fortra/impacket",
+    "https://github.com/andrew-d/static-binaries",
+    "https://github.com/dirkjanm/BloodHound.py",
+    "https://github.com/ly4k/Certipy",  # Certipy repo
+    "https://github.com/p0dalirius/Coercer"  # Coercer repo
+]
+ 
+# Directory to clone repositories into
+clone_dir = "/opt"
+ 
+# Ensure the /opt directory exists
+if not os.path.exists(clone_dir):
+    os.makedirs(clone_dir)
+ 
+# Clone each repository
+for repo in repositories:
+    repo_name = repo.split("/")[-1]
+    clone_path = os.path.join(clone_dir, repo_name)
+    # Construct the git clone command
+    clone_command = f"git clone {repo} {clone_path}"
+    print(f"Cloning {repo} into {clone_path}...")
+    # Run the git clone command
+    run_command(clone_command)
+ 
+# Install BloodHound.py
+bloodhound_dir = os.path.join(clone_dir, "BloodHound.py")
+print(f"Installing BloodHound.py in {bloodhound_dir}...")
+run_command("sudo python3 setup.py install", cwd=bloodhound_dir)
+ 
+# Install Coercer
+coercer_dir = os.path.join(clone_dir, "Coercer")
+print(f"Installing Coercer in {coercer_dir}...")
+run_command("sudo python3 setup.py install", cwd=coercer_dir)
+ 
+print("All repositories have been cloned and installations completed for BloodHound.py and Coercer.")
